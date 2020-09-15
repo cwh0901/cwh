f77fb412d968d9a9fce1c613438606e3a3d6eca6
cwh0901 <70868246+cwh0901@users.noreply.github.com>
2020-09-16 00:51:15 +0800

    Create 1090916.py

---
+++ 1090916.py
@@ -0,0 +1,15 @@
+#練習3
+string="I love you"
+print(string[3:6])
+
+#練習4
+strint="I love you"
+print(strint[::-1])
+
+#練習5
+print("I love you","so "*100,"much")
+
+#練習6
+import random
+x=random.randint(1,100)
+print("I love you","so "*x,"much")
