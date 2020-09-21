b7b8d21eeb1be54a985974da368bc55572a175d4
cwh0901 <70868246+cwh0901@users.noreply.github.com>
2020-09-21 08:26:15 +0800

    Add files via upload

---
+++ 玟浩第二章.py
@@ -0,0 +1,22 @@
+#練習3
+string="I love you"
+x=string[0:1]
+y=string[2:6]
+z=string[7:10]
+print(x,y,z)
+
+#練習4
+string="I love you"
+x=string[0:1]
+y=string[2:6]
+z=string[7:10]
+print(z[::-1],y[::-1],x[::-1])
+
+#練習5
+print("I love you","so "*100,"much")
+
+#練習6
+import random
+x=random.randint(1,100)
+print("I love you","so "*x,"much")
+
