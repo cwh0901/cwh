a96cb888193fcf165b8fb72539bd1d861201a99d
cwh0901 <70868246+cwh0901@users.noreply.github.com>
2020-11-23 10:07:48 +0800

    Add files via upload

--- 玟浩.py
+++ 玟浩.py
@@ -25,37 +25,51 @@ page_source = r.text
 page_source2 = page_source.split('\n')
 
 soup = BeautifulSoup(r.text, "lxml")
-
+list1=[]
+list2=[]
+list3=[]
+list4=[]
 ####################################第一種#######################################
-# tag_div=soup.find_all('div',class_="centralContent")
-# print(tag_div)
-# print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-# for a in tag_div:
-#     print(a.text)
+tag_div=soup.find_all('div',class_="centralContent")
+print(tag_div)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+for a in tag_div:
+    print(a.text)
+list1.append(a.text)
+####################################第二種#######################################
 
-# ####################################第二種#######################################
-
-# tag_div1=soup.find_all('div',class_="scrollable")
-# print(tag_div1)
-# print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-# for a in tag_div1:
-#     print(a.text)
-
+tag_div1=soup.find_all('div',class_="scrollable")
+print(tag_div1)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+for a in tag_div1:
+    print(a.text)
+list2.append(a.text)
 # ####################################第三種#######################################
-# tag_div2=soup.find_all('div',class_="page--index")
-# print(tag_div2)
-# print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-# for a in tag_div2:
-#     print(a.text)
-####################################第四種#######################################
-tag_div3=soup.find_all('div',class_="paragraph")
+tag_div2=soup.find_all('div',class_="page--index")
+print(tag_div2)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+for a in tag_div2:
+    print(a.text)
+list3.append(a.text)
+# ####################################第四種#######################################
+tag_div3=soup.find_all('div',class_="container")
 print(tag_div3)
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 for a in tag_div3:
     print(a.text)
+list4.append(a.text)
     
     
-# import csv
-# with open('1.csv','w',newline='' )as csvfile:
-#     wt=csv.writer(csvfile)
-#     wt.writerow([tag_div,tag_div1,tag_div2,tag_div3])
+import csv
+csvfile='1.csv'
+with open(csvfile,'w',newline='',encoding='utf-8-sig' )as fp:
+# with open(csvfile,'w',newline='' )as fp:
+    writer=csv.writer(fp)
+    writer.writerow([list1[0]])
+    writer.writerow([list2[0]])
+    writer.writerow([list3[0]])
+    writer.writerow([list4[0]])
+    
+
+
+    
